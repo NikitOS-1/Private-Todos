2c276f65ff79e27139b55e83fcb3ca8bc30f5a70
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-03-29 00:51:20 +0300

    add MUI and changeTheme function

--- src/App.js
+++ src/App.js
@@ -2,10 +2,18 @@ import { Route, Routes } from "react-router-dom";
 import AuthPage from "./pages/AuthPage";
 import HomePage from "./pages/HomePage";
 import RegisterPage from "./pages/RegisterPage";
+import style from "./App.module.scss";
+import { useSelector } from "react-redux";
 
 function App() {
+  const theme = useSelector((state) => state.theme.theme);
+
   return (
-    <div className="App">
+    <div
+      className={[
+        style["wrap"],
+        `${theme ? style["day"] : style["night"]}`,
+      ].join(" ")}>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<AuthPage />} />
