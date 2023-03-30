f73087438f3690182e2d767243ea667c814e2f75
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-03-30 15:08:51 +0300

    add authReducer

---
+++ src/redux/authReducer.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {};
+
+const userAuth = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {},
+});
+
+export const {} = userAuth.actions;
+export default userAuth;
