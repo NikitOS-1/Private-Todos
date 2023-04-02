caa0629cf84c44b5bd073472dcf5c4341186738f
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-04-02 17:17:19 +0300

    .

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,13 +1,16 @@
 import { Button, Switch } from "@mui/material";
 import { useSelector } from "react-redux";
 import style from "./Menu.module.scss";
+import { useState } from "react";
 
 const Menu = ({ exit, handleChangeTheme }) => {
   const theme = useSelector((state) => state.theme.theme);
+  const [email, setEmail] = useState();
 
   console.log("MENU render");
   return (
     <div className={style.wraper}>
+      {email}
       <Switch checked={theme} onChange={handleChangeTheme} color="default" />
       <Button onClick={exit}>Sign Out</Button>
     </div>
