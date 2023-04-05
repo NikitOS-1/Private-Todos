feca18ad778af885a17748d6d057a17f375df7d4
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-04-05 19:04:31 +0300

    compleated style from DescTask

--- src/components/Todos/DescTask/DescTask.jsx
+++ src/components/Todos/DescTask/DescTask.jsx
@@ -1,8 +1,26 @@
+import { Tab, Tabs } from "@mui/material";
+import style from "./DescTask.module.scss";
+import { useState } from "react";
+
 const DescTask = ({ changeStatusOn }) => {
+  const [value, setValue] = useState(2);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
   return (
-    <div>
-      <button onClick={() => changeStatusOn("active")}>Active</button>
-      <button onClick={() => changeStatusOn("completed")}>Completed</button>
+    <div className={style.wrap}>
+      <Tabs className={style.cont} value={value} onChange={handleChange}>
+        <Tab
+          className={style.btnAct}
+          label="Active"
+          onClick={() => changeStatusOn("active")}
+        />
+        <Tab
+          className={style.btnCompl}
+          label="Completed"
+          onClick={() => changeStatusOn("completed")}
+        />
+      </Tabs>
     </div>
   );
 };
